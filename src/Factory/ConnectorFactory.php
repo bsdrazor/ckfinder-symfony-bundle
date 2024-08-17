a2052723b538f742ff1932a4d8d01339c46b6133
Yonchi <44063781+bsdrazor@users.noreply.github.com>
2024-08-17 13:25:16 +0300

    Add files via upload

---
+++ src/Factory/ConnectorFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CKSource\Bundle\CKFinderBundle\Factory;
+
+use CKSource\Bundle\CKFinderBundle\Authentication\AuthenticationInterface;
+use CKSource\CKFinder\CKFinder;
+use Psr\Container\ContainerInterface;
+"friendsofphp/php-cs-fixer": "^3.11",
+class ConnectorFactory
+{
+    protected ?CKFinder $connectorInstance = null;
+
+    /**
+     * @param array<string,mixed> $connectorConfig
+     */
+    public function __construct(
+        protected array $connectorConfig,
+        protected AuthenticationInterface $authenticationService,
+        protected ContainerInterface $servicesMap,
+    ) {
+    }
+
+    public function getConnector(): CKFinder
+    {
+        if (null !== $this->connectorInstance) {
+            return $this->connectorInstance;
+        }
+
+        /** @var CKFinder $connector */
+        $connector = new $this->connectorConfig['connectorClass']($this->connectorConfig);
+        $connector['authentication'] = $this->authenticationService;
+        $connector['services_map'] = fn (): \Psr\Container\ContainerInterface => $this->servicesMap;
+
+        return $this->connectorInstance = $connector;
+    }
+}
---
+++ src/Form/Type/CKFinderFileChooserType.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * This file is a part of the CKFinder bundle for Symfony.
+ *
+ * Copyright (C) 2016, CKSource - Frederico Knabben. All rights reserved.
+ *
+ * Licensed under the terms of the MIT license.
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace CKSource\Bundle\CKFinderBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CKFinderFileChooserType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'button_text' => 'Browse',
+            'button_attr' => [],
+            'mode' => 'popup',
+        ]);
+
+        $allowedTypes = [
+            'button_text' => 'string',
+            'button_attr' => 'array',
+            'mode' => 'string',
+        ];
+
+        foreach ($allowedTypes as $option => $allowedType) {
+            $resolver->addAllowedTypes($option, $allowedType);
+        }
+
+        $resolver->setAllowedValues('mode', ['popup', 'modal']);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $view->vars['button_text'] = $options['button_text'];
+        $view->vars['button_attr'] = $options['button_attr'];
+        $view->vars['mode'] = $options['mode'];
+        $view->vars['button_id'] = 'ckf_filechooser_' . $view->vars['id'];
+    }
+
+    public function getParent(): string
+    {
+        return TextType::class;
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'ckfinder_file_chooser';
+    }
+}
